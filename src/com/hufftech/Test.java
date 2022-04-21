67434a90172a991aeb863cf8db03c8528a169bac
ahuffman <ahuffman9000@gmail.com>
2022-04-21 18:53:53 -0400

    Simple blanking game implemented.

--- src/com/hufftech/Test.java
+++ src/com/hufftech/Test.java
@@ -1,17 +1,46 @@
 package com.hufftech;
 
+import com.hufftech.gameplay.MemorizationGamePlayer;
+import com.hufftech.memorization.BlankingMemorizationGame;
+import com.hufftech.memorization.MemorizationGame;
 import com.hufftech.passage.Passage;
 import com.hufftech.passage.StringPassage;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        Passage p = new StringPassage("This is my passage: Hello World!");
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("\"" + p + "\"");
+        System.out.print("Enter the passage to memorize: ");
+        String input = s.nextLine();
 
-        for (String word : p) {
-            System.out.println("\"" + word + "\"");
+        Passage myPassage = new StringPassage(input);
+        BlankingMemorizationGame myGame = new BlankingMemorizationGame(myPassage);
+        MemorizationGamePlayer player = new MemorizationGamePlayer(myGame);
+
+        while (!myGame.fullyBlanked()) {
+            System.out.println("Turn " + player.getTurn());
+            System.out.println(myGame);
+            System.out.println();
+            System.out.print(">>> ");
+
+            input = s.nextLine();
+            boolean result = player.next(input);
+
+            if (result) {
+                System.out.println("Hooray!!");
+            }
+            else {
+                System.out.println("Ohhh...");
+            }
+
+            System.out.println();
         }
 
+        System.out.println("Statistics: ");
+        System.out.println("\tTurns taken: " + player.getTurn());
+        System.out.println("\tSuccesses: " + player.getSuccesses());
+        System.out.println("\tFails: " + player.getFails());
     }
 }
---
+++ src/com/hufftech/gameplay/MemorizationGamePlayer.java
@@ -0,0 +1,71 @@
+package com.hufftech.gameplay;
+
+import com.hufftech.memorization.MemorizationGame;
+
+/**
+ * A simple "scoring" class which keeps track of play data about a certain memorization game.
+ */
+public class MemorizationGamePlayer {
+
+    /**
+     * Constructor.
+     * @param game The game to track.
+     */
+    public MemorizationGamePlayer(MemorizationGame game) {
+        if (game == null) {
+            throw new NullPointerException("Argument 'game' cannot be null.");
+        }
+
+        _game = game;
+    }
+
+    /**
+     * Advances to the next turn, based upon the user's input.
+     * @param input The user's answer.
+     * @return Whether this turn was a success or a fail.
+     */
+    public boolean next(String input) {
+        boolean result = _game.matches(input);
+
+        if (result) {
+            successes++;
+            _game.next();
+        }
+        else {
+            fails++;
+        }
+
+        turn++;
+        return result;
+    }
+
+    /**
+     * Gets the number of turns which have been taken.
+     * @return the number of turns.
+     */
+    public int getTurn() {
+        return turn;
+    }
+
+    /**
+     * Gets the number of successes the user has had.
+     * @return the number of successes.
+     */
+    public int getSuccesses() {
+        return successes;
+    }
+
+    /**
+     * Gets the number of fails the user has had.
+     * @return the number of fails.
+     */
+    public int getFails() {
+        return fails;
+    }
+
+
+    private final MemorizationGame _game;
+    private int turn = 0;
+    private int successes = 0;
+    private int fails = 0;
+}
